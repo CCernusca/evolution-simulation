dfecdd0921e35edaee6d0e393261801c5587ac56
CCernusca <cernusca.c22@htlwienwest.at>
2024-12-04 11:18:46 +0100

    Added docstrings - Added more detailed docstrings to functions

--- analysis/main.py
+++ analysis/main.py
@@ -1,11 +1,20 @@
 
 # Imports
-...
+from ..simulation import main as simulation
+from ..console import main as console
 
 # Functions
 def main() -> None:
     """
     Main function of the analysis subdirectory, managing the modules responsible for data analysis of the simulations.
+
+    Parameters
+    ----------
+        None
+        
+    Returns
+    -------
+        None
     """
     ...
 
